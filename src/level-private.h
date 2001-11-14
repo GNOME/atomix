73437ab382cd749c3f006e0de301c51545093120
Jens Finke <jens@triq.net>
2001-11-14 15:07:43 +0000

    Forgot to add this. So here it is.
    2001-11-14  Jens Finke <jens@triq.net>
    
    	* level-private.h: Forgot to add this. So here it is.

---
+++ src/level-private.h
@@ -0,0 +1,37 @@
+/* Atomix -- a little mind game about atoms and molecules.
+ * Copyright (C) 2001 Jens Finke
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef _ATOMIX_LEVEL_PRIVATE_H_
+#define _ATOMIX_LEVEL_PRIVATE_H_
+
+#include "playfield.h"
+
+struct _LevelPrivate {
+	gchar*       name;         /* name of the level */
+	PlayField*   goal;         /* determines the end of the level */
+	PlayField*   playfield;    /* starting situation */
+
+	/* the following fields are only used by atomixed */
+	gchar *file_name;          /* file name of the level */
+	gboolean modified;         /* whether the level is modified */
+};
+
+Level* level_new (void);
+
+
+#endif /* _ATOMIX_LEVEL_PRIVATE_H_ */
